f3f4109bf19b31a48f48786cca5f299f5b9c66f1
Gustavo Rodrigues <omykronbr@gmail.com>
2025-04-17 13:56:26 -0400

    maybe?

--- ios/Classes/LibphonenumberPlugin.m
+++ ios/Classes/LibphonenumberPlugin.m
@@ -13,7 +13,7 @@
                                                                 binaryMessenger:[registrar messenger]];
     
     LibphonenumberPlugin* instance = [[LibphonenumberPlugin alloc] init];
-    instance.phoneUtil = [[NBPhoneNumberUtil alloc] sharedInstance];
+    instance.phoneUtil = [[NBPhoneNumberUtil sharedInstance];
     
     [registrar addMethodCallDelegate:instance channel:channel];
 }
